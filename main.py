4b9fbc6f5f040ebe4642201018cb4789f18cd8c3
iBrahimHammadi <ibrahimsh2091@gmail.com>
2023-09-25 22:49:38 +0100

    Added a class player, but there are alot of mistakes to fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 
 #Creating the Window screen
@@ -7,6 +8,33 @@ Screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Invaders Game')
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
+
+#Creating our player
+class Player():
+
+    def __init__(self, x, y, name, x_vel):
+        self.x = x
+        self.y = y
+        self.x_vel = x_vel
+        Screen.blit(pygame.image.load(name),(x, y))
+        self.loop()
+    
+    def move(self, dx):
+        self.x += dx
+
+
+    def loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.move(-0.1)
+                if event.key == pygame.K_RIGHT:
+                    self.move(0.1)
+                else:
+                    self.move(0)
+            pygame.display.update()
+
+
 
 #Creating the game loop
 run = True
@@ -17,4 +45,10 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             break
+    Screen.fill((0,0,0))
+
+    player = Player(368, 500,'player1.png', 0)
+    
+
+    pygame.display.update()
 pygame.quit()
